b321183567fbdbe33f7dc968e74f92e92984763c
Yuki123456788 <76519537+Yuki123456788@users.noreply.github.com>
2022-01-25 22:27:50 +0800

    Delete MasterTime directory

--- MasterTime/start.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-import sys
-from Controller import MainWindow_controller
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow_controller()
-    window.show()
-    sys.exit(app.exec_())
